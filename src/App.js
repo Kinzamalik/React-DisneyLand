41e88d28166bc4c037501d946b786cc48bd0bc88
Kinzamalik <kiinza.malik@gmail.com>
2021-06-05 13:21:14 +0500

    using styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login'
+
+
 
 function App() {
   return (
     <div className="App">
-   <h1>Welcome to Disney Land 🚀</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
